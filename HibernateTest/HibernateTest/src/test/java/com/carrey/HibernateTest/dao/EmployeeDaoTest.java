3828c125dce4653adbab1b92c2bd1d9bc0213d06
bonn19573 <bonn19573@hotmail.com>
2016-02-20 19:46:55 +0800

    JPA test

---
+++ HibernateTest/HibernateTest/src/test/java/com/carrey/HibernateTest/dao/EmployeeDaoTest.java
@@ -0,0 +1,54 @@
+package com.carrey.HibernateTest.dao;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+import com.carrey.HibernateTest.Certificate;
+import com.carrey.HibernateTest.Employee;
+
+public class EmployeeDaoTest extends AbstractTest {
+
+	private EmployeeDao dao = new EmployeeDao();
+	@Test
+	public void testInsert() {
+//		Employee employee= new Employee("Guo", "Rui", 30);
+//		Certificate certificate = new Certificate("java");
+//		
+//		Set<Certificate> set = new HashSet<Certificate>();
+//		set.add(certificate);
+//		employee.setCertificates(set);
+//		
+//		dao.insert(employee);
+//		
+//		System.out.println(employee);
+	}
+	
+	@Test
+	public void testSaveEmployee(){
+		Employee employee= new Employee("Guo", "Rui", 30);
+		Certificate certificate = new Certificate("java");
+		
+		Set<Certificate> set = new HashSet<Certificate>();
+		set.add(certificate);
+		employee.setCertificates(set);
+		
+		dao.saveEmployee(employee);
+		
+		System.out.println(employee);
+	}
+
+	@Test
+	public void testDelete() {
+	}
+
+	@Test
+	public void testUpdate() {
+	}
+
+	@Test
+	public void testGet() {
+	}
+
+}
